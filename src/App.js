4a8f5ac9adae8888710e13ac111227d0d1d2f0b5
Mikaela Rodriguez <mikrdgz@gmail.com>
2019-09-09 14:04:05 -0500

    jljk

--- src/App.js
+++ src/App.js
@@ -8,17 +8,6 @@ const service = createCheckoutService();
 
 
 
-embedCheckout({
-  "containerId": "bc-embedded-checkout",
-  "url": "https://mikaelarodriguez.com/cart.php?embedded=1&action=loadInCheckout&id=c9559566-9c2d-470e-9ff3-62feb6cc70b5&token=c10526531c996fb90ec910ccc21aa175f6ba4319d77bcda77f8f118fd81183d2",
-  "styles": {
-    "button": {
-      "backgroundColor": "#ffffff",
-      "borderColor": "#000000",
-      "color": "#000000"
-    }
-  }
-});
 
 
 function App() {
