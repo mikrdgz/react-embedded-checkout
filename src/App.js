6519cece98e0cc73b45f813a41dfa1ac5975aabd
Mikaela Rodriguez <mikrdgz@gmail.com>
2019-09-09 13:44:03 -0500

    embed in scope

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {createCheckoutService} from '@bigcommerce/checkout-sdk'
+import {embedCheckout} from '@bigcommerce/checkout-sdk'
 
 const service = createCheckoutService();
 
 
 
-service.embedCheckout({
+
+embedCheckout({
   "containerId": "bc-embedded-checkout",
   "url": "https://mikaelarodriguez.com/cart.php?embedded=1&action=loadInCheckout&id=c9559566-9c2d-470e-9ff3-62feb6cc70b5&token=c10526531c996fb90ec910ccc21aa175f6ba4319d77bcda77f8f118fd81183d2",
   "styles": {
