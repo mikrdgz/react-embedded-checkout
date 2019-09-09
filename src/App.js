b6436f47350bcf7bc2e580c8d518dfa92b580530
Mikaela Rodriguez <mikrdgz@gmail.com>
2019-09-09 13:12:10 -0500

    instantiate checkout service

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import logo from './logo.svg';
 import './App.css';
 import { createCheckoutService } from '@bigcommerce/checkout-sdk';
 
+const service = createCheckoutService();
+
+
 embedCheckout({
 "containerId": "bc-embedded-checkout",
 "url": "https://mikaelarodriguez.com/cart.php?embedded=1&action=loadInCheckout&id=c9559566-9c2d-470e-9ff3-62feb6cc70b5&token=c10526531c996fb90ec910ccc21aa175f6ba4319d77bcda77f8f118fd81183d2",
