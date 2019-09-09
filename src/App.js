7b9d5ff87db3880252f9285cc531f377e8878aa6
Mikaela Rodriguez <mikrdgz@gmail.com>
2019-09-09 14:20:19 -0500

    klhl

--- src/App.js
+++ src/App.js
@@ -7,21 +7,23 @@ const service = createCheckoutService();
 
 
 
-function App() {
-  
-  return (
-    embedCheckout({
-      "containerId": "bc-embedded-checkout",
-      "url": "https://mikaelarodriguez.com/cart.php?embedded=1&action=loadInCheckout&id=c9559566-9c2d-470e-9ff3-62feb6cc70b5&token=c10526531c996fb90ec910ccc21aa175f6ba4319d77bcda77f8f118fd81183d2",
-      "styles": {
-        "button": {
-          "backgroundColor": "#ffffff",
-          "borderColor": "#000000",
-          "color": "#000000"
-        }
+class App extends React.Component {
+componentDidMount(){
+  embedCheckout({
+    "containerId": "bc-embedded-checkout",
+    "url": "https://mikaelarodriguez.com/cart.php?embedded=1&action=loadInCheckout&id=c9559566-9c2d-470e-9ff3-62feb6cc70b5&token=c10526531c996fb90ec910ccc21aa175f6ba4319d77bcda77f8f118fd81183d2",
+    "styles": {
+      "button": {
+        "backgroundColor": "#ffffff",
+        "borderColor": "#000000",
+        "color": "#000000"
       }
-    })
-  );
+    }
+  })
+ }
+ render(){
+   return <h1>BC SDK</h1>
+ }
 }
 
 export default App;
