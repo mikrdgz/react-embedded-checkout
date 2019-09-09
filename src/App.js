5c46ad65af9b14d234898b4e634f01a5e702c346
Mikaela Rodriguez <mikrdgz@gmail.com>
2019-09-09 14:11:56 -0500

    sjhal

--- src/App.js
+++ src/App.js
@@ -7,23 +7,20 @@ const service = createCheckoutService();
 
 
 
-
-
-
 function App() {
-  embedCheckout({
-    "containerId": "bc-embedded-checkout",
-    "url": "https://mikaelarodriguez.com/cart.php?embedded=1&action=loadInCheckout&id=c9559566-9c2d-470e-9ff3-62feb6cc70b5&token=c10526531c996fb90ec910ccc21aa175f6ba4319d77bcda77f8f118fd81183d2",
-    "styles": {
-      "button": {
-        "backgroundColor": "#ffffff",
-        "borderColor": "#000000",
-        "color": "#000000"
-      }
-    }
-  });
+  
   return (
-    <div id="bc-embedded-checkout"></div>
+    embedCheckout({
+      "containerId": "bc-embedded-checkout",
+      "url": "https://mikaelarodriguez.com/cart.php?embedded=1&action=loadInCheckout&id=c9559566-9c2d-470e-9ff3-62feb6cc70b5&token=c10526531c996fb90ec910ccc21aa175f6ba4319d77bcda77f8f118fd81183d2",
+      "styles": {
+        "button": {
+          "backgroundColor": "#ffffff",
+          "borderColor": "#000000",
+          "color": "#000000"
+        }
+      }
+    })
   );
 }
 
