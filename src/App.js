2db1d2efbcd214056266a24dd3c3856be4a88b8b
Mikaela Rodriguez <mikrdgz@gmail.com>
2019-09-12 14:25:36 -0500

    it works

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-//import {createCheckoutService} from '@bigcommerce/checkout-sdk'
+import {createCheckoutService} from '@bigcommerce/checkout-sdk'
 import {embedCheckout} from '@bigcommerce/checkout-sdk'
 
 const service = createCheckoutService();
