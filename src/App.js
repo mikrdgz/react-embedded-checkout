1ebd19931aff86f6b3e2f35c745aef2f7687d64d
Mikaela Rodriguez <mikrdgz@gmail.com>
2019-09-12 14:19:13 -0500

    jlkj

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
-import {createCheckoutService} from '@bigcommerce/checkout-sdk'
-//import {embedCheckout} from '@bigcommerce/checkout-sdk'
+//import {createCheckoutService} from '@bigcommerce/checkout-sdk'
+import {embedCheckout} from '@bigcommerce/checkout-sdk'
 
 const service = createCheckoutService();
 
